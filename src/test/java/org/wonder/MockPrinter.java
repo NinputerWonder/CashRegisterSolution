b549a71b86e4bce30ca3e4e30a53e25c08fb534d
NinputerWonder <ninputewonder@outlook.com>
2020-08-27 11:36:23 +0800

    1.2 verify print real purchase, STEP 2, verify the argument

--- src/test/java/org/wonder/MockPrinter.java
+++ src/test/java/org/wonder/MockPrinter.java
@@ -1,20 +1,27 @@
 package org.wonder;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class MockPrinter extends Printer {
 
     private boolean isInvoked;
+    private String actualPurchaseDescription;
+
     public MockPrinter(){
         isInvoked = false;
+        actualPurchaseDescription = null;
     }
 
     @Override
     public void print(String actualPurchaseDescription){
         isInvoked = true;
+        this.actualPurchaseDescription = actualPurchaseDescription;
     }
 
     public void verifyThatPrintWasCalledWith(String expectedPurchaseDescription) {
         assertTrue("expected print to be called at least once, print was not called", isInvoked);
+        assertEquals("expected print to be called with '" + expectedPurchaseDescription + "', but was called with '" + actualPurchaseDescription + "'",
+                expectedPurchaseDescription, actualPurchaseDescription);
     }
 }
